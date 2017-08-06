0bbef8734152716686e9913a3cae64514c7fce9a
Adam L Marsh <am@ui-design-engineering.com>
2017-08-06 12:12:16 -0700

    setting up

---
+++ test.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>
+Template Start Page
+</title>
+
+
+
+<style type="text/css">
+.message {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid blue;
+  background: #CCCCCC;
+}
+
+.footer {
+  font-size: small;
+  font-style: italic;
+}
+
+</style>
+</head>
+
+<body>
+
+<h1><?php echo '<p>Hello World (written in php on a php page).</p>'; ?> </h1>
+<p>
+Testing ... localhost/chichibug
+</p>
+
+<div>
+The README is available <a href="README">here</a>.
+</div>
+
+<hr/>
+
+<div class="footer">
+Copyright &copy; 2017
+<a href="http://www.ui-design-engineering.com">Adam Marsh: www.ui-design-engineering.com</a>.
+All rights reserved.<br/>
+
+</div>
+</body>
+
+</html>
